6ddf92ad1bc8f28ec890de96828d86ca0732986c
Josiah King <llvll@Josiahs-MacBook-Air.local>
2024-11-27 09:26:32 -0400

    added edit constraints however code needs cleaning up and is error prone

--- static/javascript/constraints.js
+++ static/javascript/constraints.js
@@ -4,7 +4,10 @@ const errorTag = document.getElementById("error-tag")
 const BTNCLICK = localStorage.getItem("buttonClicked")
 const notesSection = document.getElementById("notes-section")
 const editSection = document.getElementById("edit-section")
-let worker1Name,worker2Name
+const worker1Data = JSON.parse(localStorage.getItem("worker1Data"))
+const worker2Data = JSON.parse(localStorage.getItem("worker2Data"))
+const notes = localStorage.getItem("notes")
+let constraintId,oldNotes
 let PHASE = 1
 
 const btnInnerHtml = 
@@ -19,7 +22,7 @@ if (BTNCLICK === 'edit-constraint'){
 
 document.getElementById("submit-btn").innerHTML = btnInnerHtml
 
-function populateTable(workerObj,workerTable,name){
+function populateTable(workerObj,workerTable,inputName,idParam,inputClass=""){
     for (const data of workerObj){
         const workerRow = document.createElement("tr")
         const firstName = document.createElement("td")
@@ -28,7 +31,7 @@ function populateTable(workerObj,workerTable,name){
         const location = document.createElement("td")
         const radioInput = document.createElement("input")
         const label = document.createElement("label")
-        const radioId = `worker1-${data.id}`
+        const radioId = `i${idParam}-${data.id}`
 
         firstName.innerHTML = data.first_name
         lastName.innerHTML = data.last_name
@@ -38,7 +41,8 @@ function populateTable(workerObj,workerTable,name){
         radioInput.setAttribute("id",radioId)
         radioInput.setAttribute("type","radio")
         radioInput.setAttribute("value",data.id)
-        radioInput.setAttribute("name",name+'-option')
+        radioInput.setAttribute("name",inputName+'-option')
+        if (inputClass)radioInput.classList.add(inputClass)
 
         label.setAttribute("for",radioId)
         label.innerHTML = "Select"
@@ -54,18 +58,14 @@ function populateTable(workerObj,workerTable,name){
 }
 
 window.addEventListener("DOMContentLoaded", async function () {
-    const worker1Data = JSON.parse(localStorage.getItem("worker1Data"))
-    const worker2Data = JSON.parse(localStorage.getItem("worker2Data"))
-    const notes = localStorage.getItem("notes")
-
     const notesTextarea = document.getElementById("add-notes-textarea")
     notesTextarea.innerHTML = notes
 
     const worker1Table = document.getElementById("worker1-table")
     const worker2Table = document.getElementById("worker2-table")
 
-    populateTable(worker1Data,worker1Table,"worker1")
-    populateTable(worker2Data,worker2Table,"worker2")
+    populateTable(worker1Data,worker1Table,"worker1","worker1")
+    populateTable(worker2Data,worker2Table,"worker2","worker2")
 })
 
 document.getElementById("form").addEventListener("submit",async function(event){
@@ -77,6 +77,7 @@ document.getElementById("form").addEventListener("submit",async function(event){
     if (PHASE === 1){
         const inputTags = document.querySelectorAll("input")
         const notes = document.getElementById("add-notes-textarea").value
+        oldNotes = notes
         let worker1Check,worker2Check = true
         let worker1,worker2
 
@@ -112,6 +113,8 @@ document.getElementById("form").addEventListener("submit",async function(event){
 
         const worker1ErrorTag = document.getElementById("worker1-error-tag")
         const worker2ErrorTag = document.getElementById("worker2-error-tag")
+        const worker1ResultTable = document.getElementById("worker1-result-table")
+        const worker2ResultTable = document.getElementById("worker2-result-table")
 
         const worker1Results = await findWorker(worker1Firstname,worker1Lastname)
         const worker2Results = await findWorker(worker2Firstname,worker2Lastname)
@@ -124,6 +127,44 @@ document.getElementById("form").addEventListener("submit",async function(event){
             return
         }
 
+        for (const tag of [worker1ResultTable,worker2ResultTable]){
+            tag.classList.remove("hidden")
+        }
+
+        populateTable(worker1Results,worker1ResultTable,"worker1-result","worker1-result","results")
+        populateTable(worker2Results,worker2ResultTable,"worker2-result","worker2-result","results")
+        PHASE = 3
+        
+    }else if (PHASE === 3){
+        const resultInputs = document.getElementsByClassName("results")
+        let worker1ResultCheck = true,worker2ResultCheck = true
+        let notesValue = document.getElementById('edit-notes-textarea').value
+        let newWorker1Id,newWorker2Id
+
+        for (const input of resultInputs){
+            if (input.checked){
+                if (input.name === "worker1-result-option"){
+                    console.log('passed in one')
+                    worker1ResultCheck = false
+                    newWorker1Id = parseInt(input.value)
+                }else{
+                    console.log(input.name)
+                    worker2ResultCheck = false
+                    newWorker2Id = parseInt(input.value)
+                }
+            }
+        }
+
+        const newNotes = notesValue ? notesValue : oldNotes
+
+        if (worker1ResultCheck || worker2ResultCheck){
+            errorTag.innerHTML = "Please insert all values"
+            return
+        }
+
+        const results = await editConstraints(newWorker1Id,newWorker2Id,newNotes,'edit',constraintId)
+
+        console.log(results)
         
     }
 })
@@ -155,13 +196,12 @@ async function modifyConstraint(worker1,worker2,notes){
         data = results2[0]
     }else{
         errorTag.innerHTML = "Constraint does not exist!"
-        return 2 //for phase
+        return
     }
 
+    constraintId = data.id
     textArea.setAttribute("placeholder",notes)
-
     editSection.classList.remove("hidden")
-
     return
 }
 
